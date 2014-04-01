930eb7790d86afd9319fa13c38adb0c54c48d519
mudrutom <mudrutom@gmail.com>
2014-04-02 00:16:45 +0200

    added Apache Lucene, used Czech analyzer and stemmer in mapper

--- src/main/java/cz/cvut/bigdata/wordcount/WordCountMapper.java
+++ src/main/java/cz/cvut/bigdata/wordcount/WordCountMapper.java
@@ -3,6 +3,11 @@ package cz.cvut.bigdata.wordcount;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.cz.CzechAnalyzer;
+import org.apache.lucene.analysis.cz.CzechStemmer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
 
 import java.io.IOException;
 
@@ -14,15 +19,41 @@ import java.io.IOException;
  */
 public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+	private final CzechAnalyzer analyzer = new CzechAnalyzer(Version.LUCENE_47);
+	private final CzechStemmer stemmer = new CzechStemmer();
 	private final IntWritable ONE = new IntWritable(1);
 	private final Text word = new Text();
 
 	@Override
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-		final String[] words = value.toString().split(" ");
-		for (String term : words) {
-			word.set(term);
-			context.write(word, ONE);
+		TokenStream tokenStream = null;
+		try {
+			// instantiate and reset the token stream
+			tokenStream = analyzer.tokenStream("word", value.toString());
+			tokenStream.reset();
+
+			// store the attribute
+			final CharTermAttribute termAttribute = tokenStream.getAttribute(CharTermAttribute.class);
+			// process all tokens
+			while (tokenStream.incrementToken()) {
+				int len = stemmer.stem(termAttribute.buffer(), termAttribute.length());
+				String term = termAttribute.toString().substring(0, len);
+				word.set(term);
+				context.write(word, ONE);
+			}
+		} finally {
+			// clean-up
+			finish(tokenStream);
+		}
+	}
+
+	private static void finish(TokenStream tokenStream) throws IOException {
+		if (tokenStream != null) {
+			try {
+				tokenStream.end();
+			} finally {
+				tokenStream.close();
+			}
 		}
 	}
 }
