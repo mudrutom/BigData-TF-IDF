d3aa482a8cd078fb8a70df6e15da7fc85a8f422f
mudrutom <mudrutom@gmail.com>
2014-04-02 10:50:43 +0200

    improvements of term analyzer in mapper

--- src/main/java/cz/cvut/bigdata/wordcount/WordCountMapper.java
+++ src/main/java/cz/cvut/bigdata/wordcount/WordCountMapper.java
@@ -5,11 +5,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cz.CzechAnalyzer;
-import org.apache.lucene.analysis.cz.CzechStemmer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Receives (byteOffsetOfLine, textOfLine), note we do not
@@ -19,32 +20,49 @@ import java.io.IOException;
  */
 public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+	// Lucene Czech analyzer:
+	//  standard filter > lower case filter > stop filter > czech stem filter
 	private final CzechAnalyzer analyzer = new CzechAnalyzer(Version.LUCENE_47);
-	private final CzechStemmer stemmer = new CzechStemmer();
+
 	private final IntWritable ONE = new IntWritable(1);
 	private final Text word = new Text();
 
 	@Override
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		for (String term : parseTerms(value.toString())) {
+			word.set(term);
+			context.write(word, ONE);
+		}
+	}
+
+	protected List<String> parseTerms(String text) throws IOException {
+		final List<String> result = new LinkedList<String>();
+
+		// remove HTML tags, dots and commas
+		text = text.replaceAll("<.*?>", "").replaceAll("[.]", "").replaceAll(",", "");
+
 		TokenStream tokenStream = null;
 		try {
 			// instantiate and reset the token stream
-			tokenStream = analyzer.tokenStream("word", value.toString());
+			tokenStream = analyzer.tokenStream("word", text);
 			tokenStream.reset();
 
-			// store the attribute
+			// store term attribute
 			final CharTermAttribute termAttribute = tokenStream.getAttribute(CharTermAttribute.class);
 			// process all tokens
 			while (tokenStream.incrementToken()) {
-				int len = stemmer.stem(termAttribute.buffer(), termAttribute.length());
-				String term = termAttribute.toString().substring(0, len);
-				word.set(term);
-				context.write(word, ONE);
+				String term = termAttribute.toString();
+				// exclude single characters and numbers
+				if (term.length() > 1 && !term.matches("^\\d*$")) {
+					result.add(term);
+				}
 			}
 		} finally {
 			// clean-up
 			finish(tokenStream);
 		}
+
+		return result;
 	}
 
 	private static void finish(TokenStream tokenStream) throws IOException {
